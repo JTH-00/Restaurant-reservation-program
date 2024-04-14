43032ae2c12c774ddd47dfd3fe3051294ad7a35b
JTH-00 <dbzgtsa@gmail.com>
2024-04-14 10:26:34 +0900

    방문 예약 관련 기능 추가
    방문 예약 신청 리스트,
    수락한 방문 예약 리스트,
    거절, 수락 (알람기능까지 추가예정)

--- JIN/Project_File/rest-reserve/src/main/java/com/proj/restreserve/visit/service/VisitService.java
+++ JIN/Project_File/rest-reserve/src/main/java/com/proj/restreserve/visit/service/VisitService.java
@@ -13,6 +13,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -47,5 +49,44 @@ public class VisitService {
         visit.setRestaurant(restaurant.get());
         //엔티티 클래스의 DynamicInsert로 visitcheck는 false로 설정
         this.visitRepository.save(visit);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Visit> showVisitReserve(){//방문 예약 신청 리스트
+        User user = getCurrentUser();
+        Restaurant restaurant = restaurantRepository.findByUser(user);
+        List<Visit> visits;
+        if(restaurant!=null){
+            visits = visitRepository.findByVisitcheckFalseAndRestaurant(restaurant);
+        }else{
+            throw new RuntimeException("로그인한 유저의 매장정보가 없습니다.");
+        }
+        return visits;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Visit> showPermitVisitReserve(){//수락한 방문 예약 리스트
+        User user = getCurrentUser();
+        Restaurant restaurant = restaurantRepository.findByUser(user);
+        List<Visit> visits;
+        if(restaurant!=null){
+            LocalDateTime localDateTime= LocalDateTime.now().plusMinutes(30);//입장시간이 현재시간 + 30분 전의 예약한 방문 리스트 조회
+            visits = visitRepository.findByVisitcheckTrueAndRestaurantAndVisittimeBefore(restaurant, localDateTime);
+        }else{
+            throw new RuntimeException("로그인한 유저의 매장정보가 없습니다.");
+        }
+        return visits;
+    }
+    @Transactional
+    public void refuseVisit(String visitid){//방문 예약 거절
+        //알람 작성해서 전송하는거 추가해야함
+        visitRepository.deleteById(visitid);
+    }
+    
+    @Transactional
+    public void acceptVisit(String visitid){//방문 예약 수락
+        Visit visit = visitRepository.getReferenceById(visitid);
+        visit.setVisitcheck(true);
+        //알람 보내기 해야함
     }
 }
